85a50b6a33ad0bb3f146e8ecaaf2fad51a7e2726
Garrett <stmichaelboy@icloud.com>
2026-04-08 19:33:07 -0400

    Update working_main.c

--- final_project/working_main.c
+++ final_project/working_main.c
@@ -1,12 +1,23 @@
-	  ////////////////////task 1:
-	  /*
+  /*/////////////
+	  task1
 	  for (int i = 0; i<9; i++)
 	  {
 		  sevenseg_fn(i);
 	  }
 	  */
-	  /////////////////////////////
 
-	 //task2 blink
+	  /*/////////
+	 	 task2 blink
 	  uint8_t input = keypad_reading();
 	  blink(input);
+
+	  *///////////////
+	  //task3 basic combo
+	  /*
+	  uint8_t input = keypad_reading();
+	  keypad27seg(input);
+	  */
+
+
+	  //task4 precision5
+	  precision5();
