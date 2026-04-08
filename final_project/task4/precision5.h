1889aa40f27d2219b2242326dab55e96aa945242
Garrett <stmichaelboy@icloud.com>
2026-04-08 19:41:09 -0400

    Enhance precision5 function with button handling
    Refactor precision5 function to include button state check and update LED behavior based on button press.

--- final_project/task4/precision5.h
+++ final_project/task4/precision5.h
@@ -1,5 +1,7 @@
 void precision5 (void)
 {
+	GPIO_PinState btn = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+
 	int i = 0;
 	for (int j = 0; j<5; j++)
 	{
@@ -7,17 +9,33 @@ void precision5 (void)
 		{
 			sevenseg_fn(i);
 			HAL_Delay(100);
-		} //PD0 External LED
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
-		HAL_Delay(100);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET);
-	}
-		//A0 is used as the button
-		GPIO_PinState btn = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
-		if (btn == 1 && i == 5)
-		{
-			for (int i = 0; i < 10; i++)
+			 //PD0 External LED
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
+			HAL_Delay(50);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET);
+			if(i==1)
 			{
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+			}
+			if (i==2)
+			{
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+			}
+			if (i==3)
+			{
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+			}
+			if (i==4)
+			{
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+			}
+			else if (i == 5)
+			{
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+				HAL_Delay(250);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
@@ -27,14 +45,13 @@ void precision5 (void)
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
-				HAL_Delay(250);
 			}
-		}
-		else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
-		}
+			else
+			{
+				continue;
+			}
+
+		} //i loop end
+	}// j loop end
+
 }
