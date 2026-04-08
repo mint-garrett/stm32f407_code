4a3954cf014775c425fc16588bbf01934ae61865
Garrett <stmichaelboy@icloud.com>
2026-04-08 17:14:02 -0400

    Implement blinking feature using keypad input
    Added functionality for blinking based on keypad input.

---
+++ final_project/working_main.c
@@ -0,0 +1,12 @@
+	  ////////////////////task 1:
+	  /*
+	  for (int i = 0; i<9; i++)
+	  {
+		  sevenseg_fn(i);
+	  }
+	  */
+	  /////////////////////////////
+
+	 //task2 blink
+	  uint8_t input = keypad_reading();
+	  blink(input);
