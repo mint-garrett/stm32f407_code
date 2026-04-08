a2f9aa004ee97c042949b797a51d507a77facd23
Garrett <stmichaelboy@icloud.com>
2026-04-08 16:53:21 -0400

    Update blink.c

--- final_project/task2/blink.c
+++ final_project/task2/blink.c
@@ -1,80 +1,100 @@
 //blinks inputs as prof wants them in accordance with keypad inputs
 
-void blink (uint8_t input){
-if (input == 0){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
-HAL_Delay(300);
+void blink (uint8_t input)
+{
+	if (input == 255)
+	{
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+		return;
+	}
+	else if (input == 0)
+	{
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+		HAL_Delay(300);
+	}
+	else if (input == 1)
+	{
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+		HAL_Delay(250);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+		HAL_Delay(300);
+	}
+	else if (input == 2)
+	{
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+		HAL_Delay(300);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+		HAL_Delay(300);
+	}
+	else if (input == 3)
+	{
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+		HAL_Delay(300);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+		HAL_Delay(300);
+	}
+	else if (input == 4)
+	{
+		for (uint32_t i = 0; i < 2; i++)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+			HAL_Delay(300);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+			HAL_Delay(300);
+		}
+	}
+	else if (input == 5)
+	{
+		for (uint32_t i = 0; i < 2; i++)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+			HAL_Delay(300);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+			HAL_Delay(300);
+		}
+	}
+	else if (input == 6)
+	{
+		for (uint32_t i = 0; i < 2; i++)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+			HAL_Delay(300);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+			HAL_Delay(300);
+		}
+	}
+	else if (input == 7)
+	{
+		for (uint32_t i = 0; i < 4; i++)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+			HAL_Delay(300);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+			HAL_Delay(300);
+		}
+	}
+	else if (input == 8)
+	{
+		for (uint32_t i = 0; i < 4; i++)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+			HAL_Delay(300);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+			HAL_Delay(300);
+		}
+	}
+	else if (input == 9)
+	{
+		for (uint32_t i = 0; i < 4; i++)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+			HAL_Delay(300);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+			HAL_Delay(300);
+		}
+	}
 }
-else if (input == 1){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-HAL_Delay(250);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-else if (input == 2){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-else if (input == 3){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-else if (input == 4){
-for (uint32_t i = 0; i < 2; i++){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-}
-else if (input == 5){
-for (uint32_t i = 0; i < 2; i++){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-}
-else if (input == 6){
-for (uint32_t i = 0; i < 2; i++){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-}
-else if (input == 7){
-for (uint32_t i = 0; i < 4; i++){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-}
-else if (input == 8){
-for (uint32_t i = 0; i < 4; i++){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-}
-else if (input == 9){
-for (uint32_t i = 0; i < 4; i++){
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-HAL_Delay(300);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-HAL_Delay(300);
-}
-}
-else {
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
-}
-}
+
