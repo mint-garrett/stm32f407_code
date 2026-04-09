5f532667d42adc791788f6a6b6a7d47ea512c4d6
Garrett <stmichaelboy@icloud.com>
2026-04-09 16:25:51 -0400

    Implement HAL_GPIO_EXTI_Callback for button press
    Add GPIO interrupt callback for button press handling.

--- final_project/working_main.c
+++ final_project/working_main.c
@@ -21,3 +21,28 @@
 
 	  //task4 precision5
 	  precision5();
+
+
+	//interrupt section line 331
+	void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if (GPIO_Pin == GPIO_PIN_0)
+	{
+		GPIO_PinState btn = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+		while (btn == GPIO_PIN_SET)
+		{
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+			for(int i = 0; i > DELAY; i++);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+			for(int i = 0; i > DELAY; i++);
+// update condition
+			btn = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5);
+		}
+	}
+}
