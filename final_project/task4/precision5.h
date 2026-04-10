f2186674fca0339df468eeffaf330422be4486a1
Garrett <stmichaelboy@icloud.com>
2026-04-09 20:51:43 -0400

    Refactor precision5 function for button handling

--- final_project/task4/precision5.h
+++ final_project/task4/precision5.h
@@ -1,57 +1,64 @@
 void precision5 (void)
 {
-	GPIO_PinState btn = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
-
-	int i = 0;
-	for (int j = 0; j<5; j++)
-	{
-		for (i = 0; i<10; i++)
+		for (int i = 0; i<10; i++)
 		{
 			sevenseg_fn(i);
 			HAL_Delay(100);
-			 //PD0 External LED
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
-			HAL_Delay(50);
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET);
-			if(i==1)
-			{
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-			}
-			if (i==2)
-			{
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-			}
-			if (i==3)
-			{
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-			}
-			if (i==4)
-			{
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
-			}
-			else if (i == 5)
+			GPIO_PinState btn = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+
+			if (i==0)
 			{
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
-				HAL_Delay(250);
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET);
+			}
+			else if (i==1)
+			{
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
+			}
+			else if (i==2)
+			{
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+			}
+			else if (i==3)
+			{
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+			}
+			else if(i==4)
+			{
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+			}
+			else if (i==5)
+			{
 				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
-				HAL_Delay(250);
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+				while (btn == GPIO_PIN_SET)
+				{
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET);
+					HAL_Delay(500);
+
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
+					HAL_Delay(500);
+
+					btn = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+					if(btn == GPIO_PIN_RESET || btn != GPIO_PIN_SET)
+					{
+						break;
+					}
+				}
 			}
 			else
 			{
 				continue;
 			}
-
 		} //i loop end
-	}// j loop end
-
-}
+	}
